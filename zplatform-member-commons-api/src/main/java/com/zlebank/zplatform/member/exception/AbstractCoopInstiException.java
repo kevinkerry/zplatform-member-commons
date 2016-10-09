5050beb6060afd9b38fd49320a9f7cf28b6df184
guojia <guojia@zlebank.com>
2016-10-09 17:03:00 +0800

    修改异常类

---
+++ zplatform-member-commons-api/src/main/java/com/zlebank/zplatform/member/exception/AbstractCoopInstiException.java
@@ -0,0 +1,19 @@
+package com.zlebank.zplatform.member.exception;
+
+import java.util.ResourceBundle;
+
+import com.zlebank.zplatform.member.commons.exception.AbstractDescribeException;
+
+public abstract class AbstractCoopInstiException extends AbstractDescribeException{
+
+    /**
+     * serialVersionUID
+     */
+    private static final long serialVersionUID = -6135706535011218528L;
+    
+    private static final  ResourceBundle RESOURCE = ResourceBundle.getBundle("coopinstiexception_des");
+	@Override
+	public ResourceBundle getResourceBundle() {
+		return RESOURCE;
+	}
+}
